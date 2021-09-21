76d84058e55a2efdc4e2a5d79a3cf486f87f422f
Brian <brianjamesbecker9@gmail.com>
2021-09-20 22:56:48 -0500

    removed perlin

--- Runtime/Tiles/RuleTile/RuleTile.cs
+++ Runtime/Tiles/RuleTile/RuleTile.cs
@@ -407,7 +407,7 @@ namespace UnityEngine
                             tileData.sprite = rule.m_Sprites[0];
                             break;
                         case TilingRule.OutputSprite.Random:
-                            int index = Mathf.Clamp(Mathf.FloorToInt(GetPerlinValue(position, rule.m_PerlinScale, 100000f) * rule.m_Sprites.Length), 0, rule.m_Sprites.Length - 1);
+                            int index = System.Random.Next(rule.m_Sprites.Length);
                             tileData.sprite = rule.m_Sprites[index];
                             if (rule.m_RandomTransform != TilingRule.Transform.Fixed)
                                 transform = ApplyRandomTransform(rule.m_RandomTransform, transform, rule.m_PerlinScale, position);
@@ -664,7 +664,7 @@ namespace UnityEngine
         /// <returns>A random transform matrix.</returns>
         public virtual Matrix4x4 ApplyRandomTransform(TilingRule.Transform type, Matrix4x4 original, float perlinScale, Vector3Int position)
         {
-            float perlin = GetPerlinValue(position, perlinScale, 200000f);
+            float perlin = (float) System.Random.NextDouble();
             switch (type)
             {
                 case TilingRule.Transform.MirrorXY:
